b10d8152541dd4c27a3cab55304b1ed13772b900
ZXH2y <andri9825559@gmail.com>
2026-07-01 19:29:51 +0800

    Membuat komponen card warna hitam di ProfileScreen

--- app/src/main/java/com/example/muhandriansyah223280019/ui/screen/ProfileScreen.kt
+++ app/src/main/java/com/example/muhandriansyah223280019/ui/screen/ProfileScreen.kt
@@ -6,8 +6,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.muhandriansyah223280019.ui.theme.MuhAndriansyah223280019Theme
 
 
@@ -25,6 +34,22 @@ fun ProfileScreen(){
                 )
             )
     )
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(2.dp)
+            .height(200.dp),
+        shape = RoundedCornerShape(16.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.Black.copy(alpha = 0.4f)
+        )
+    ) {
+        Text(
+            text = "hay aku andri ganteng",
+            modifier = Modifier.padding(16.dp),
+            color = Color.White
+        )
+    }
 }
 
 @Preview(showBackground = true, showSystemUi = true)
